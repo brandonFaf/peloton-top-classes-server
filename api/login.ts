31675988c215b06e10fabe221d3baf2f25cd859a
Brandon Myers <brandonstrasn91@yahoo.com>
2021-01-19 18:25:38 -0500

    change cookie samesite mode

--- api/login.ts
+++ api/login.ts
@@ -5,8 +5,13 @@ import allowCors from '../src/utils/allowCors';
 const login = async (request: NowRequest, response: NowResponse) => {
   if (request.method == 'POST') {
     const { username, password } = request.body;
+    console.log('password:', password);
+    console.log('username:', username);
     const { user_id, session_id } = await auth(username, password);
-    var setCookie = cookie.serialize('session_id', session_id);
+    var setCookie = cookie.serialize('session_id', session_id, {
+      sameSite: 'lax'
+    });
+    console.log('session_id:', session_id);
     response.setHeader('Set-Cookie', setCookie);
     response.status(200).json({ user_id, session_id });
   } else if (request.method == 'OPTIONS') {
